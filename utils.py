3fa4e9df91a4f2f3db265b9aa36d3f3ab172b0dc
Ziyad Sameh <ziyad.ss@hotmail.com>
2023-05-13 21:05:17 +0300

    Update utils.py

--- utils.py
+++ utils.py
@@ -20,13 +20,12 @@ def show_cv2_image_gray(img):
 
 
 def load_data(
-    features_function: Callable[[str], np.ndarray],
-    samples_per_label: Optional[int] = EXAMPLES_PER_LABEL,
+    features_function: Callable[[str], np.ndarray]
 ) -> tuple[list[np.ndarray], list[int]]:
     data: list[np.ndarray] = []
     labels: list[int] = []
 
-    if samples_per_label is None:
+    if EXAMPLES_PER_LABEL is None:
         with open(LABELS_FILENAME, "r") as f:
             for line in f:
                 entry = json.loads(line.strip())
@@ -47,7 +46,7 @@ def load_data(
 
                 if label not in samples_per_class:
                     samples_per_class[label] = 0
-                if samples_per_class[label] >= samples_per_label:
+                if samples_per_class[label] >= EXAMPLES_PER_LABEL:
                     continue
                 samples_per_class[label] += 1
 
