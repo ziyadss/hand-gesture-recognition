2f8b089b1279e12a153640e5f30aa963f3b98032
Ziyad Sameh <ziyad.ss@hotmail.com>
2023-05-13 20:46:09 +0300

    Update train.py

--- train.py
+++ train.py
@@ -3,10 +3,10 @@ import os
 import pickle
 import time
 
-from sklearn.model_selection import StratifiedKFold, cross_validate, train_test_split
+from sklearn.model_selection import StratifiedKFold, cross_validate
 from sklearn.svm import SVC
 
-from constants import *
+from constants import FORCE_EXTRACT, RANDOM_STATE
 from extractor import get_features_from_path
 from utils import NumpyEncoder, load_data
 
@@ -26,18 +26,13 @@ else:
     with open(DATA_FILENAME, "rb") as f:
         data, labels = pickle.load(f)
 
-X_train, X_test, y_train, y_test = train_test_split(
-    data, labels, test_size=0.2, random_state=RANDOM_STATE
-)
-
-
 clf = SVC(random_state=RANDOM_STATE, kernel="rbf")
 
 cv = StratifiedKFold(n_splits=5, shuffle=True, random_state=RANDOM_STATE)
 scores = cross_validate(
     clf,
-    X_train,
-    y_train,
+    data,  # type: ignore
+    labels,
     cv=cv,
     scoring=("accuracy", "balanced_accuracy"),
 )
@@ -46,13 +41,10 @@ scores = cross_validate(
 with open(SCORES_FILENAME, "w") as f:
     json.dump(scores, f, cls=NumpyEncoder, indent=4)
 
-clf.fit(X_train, y_train)
+clf.fit(data, labels)  # type: ignore
 
 with open(MODEL_FILENAME, "wb") as f:
     pickle.dump(clf, f)
-
-accuracy = clf.score(X_test, y_test)
-print(f"Accuracy: {accuracy*100:.1f}%")
 
 t_end = time.perf_counter()
 seconds = t_end - t_start
