c77cf2f5e88746b6819ca1319a3a9cd281d09b8e
Ziyad Sameh <ziyad.ss@hotmail.com>
2023-05-13 21:59:36 +0300

    Update features.py

--- features.py
+++ features.py
@@ -102,28 +102,20 @@ SKIP = {
 
 def segment_hand(path: str):
     image = io.imread(path)
-
     grayscale = color.rgb2gray(image)
 
-    # Apply a threshold
     threshold = filters.threshold_otsu(grayscale)
     binary = grayscale > threshold
 
-    # Remove small objects
     cleaned = morphology.remove_small_objects(binary, min_size=64, connectivity=2)
-
-    # Fill holes
     filled = morphology.remove_small_holes(cleaned, area_threshold=64, connectivity=2)
 
-    # Label regions
     labels = morphology.label(filled)
     regions = measure.regionprops(labels)
 
-    # Find the largest region
     largest_region = max(regions, key=lambda region: region.area)
     label = largest_region.label
 
-    # Extract the hand
     hand = np.zeros_like(grayscale)
     hand[labels == label] = grayscale[labels == label]
 
