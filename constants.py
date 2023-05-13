e0814a5d9f7ebb1f7c6417fc773be1397a83a313
Ziyad Sameh <ziyad.ss@hotmail.com>
2023-05-13 20:49:13 +0300

    Update constants.py

--- constants.py
+++ constants.py
@@ -2,8 +2,8 @@ import os
 
 FORCE_EXTRACT = False
 
-RANDOM_STATE = 42
-EXAMPLES_PER_LABEL = 20  # None for entire dataset
+RANDOM_STATE = 465
+EXAMPLES_PER_LABEL = None  # None for entire dataset
 
 DATA_DIRECTORY = "data"
 MEN_DIRECTORY = os.path.join(DATA_DIRECTORY, "men")
