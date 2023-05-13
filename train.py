ff5ff84dd5b48413d0b48de16b442d382a006932
Ziyad Sameh <ziyad.ss@hotmail.com>
2023-05-13 20:33:38 +0300

    Update train.py

--- train.py
+++ train.py
@@ -10,7 +10,7 @@ from constants import *
 from extractor import get_features_from_path
 from utils import NumpyEncoder, load_data
 
-t_start = time.process_time_ns()
+t_start = time.perf_counter()
 
 DATA_FILENAME = "data_train.pkl"
 MODEL_FILENAME = "model_train.pkl"
@@ -54,5 +54,7 @@ with open(MODEL_FILENAME, "wb") as f:
 accuracy = clf.score(X_test, y_test)
 print(f"Accuracy: {accuracy*100:.1f}%")
 
-t_end = time.process_time_ns()
-print(f"Time: {(t_end - t_start) / 1e9 / 60:.1f} minutes")
+t_end = time.perf_counter()
+seconds = t_end - t_start
+
+print(f"Time: {seconds:.1f} seconds")
