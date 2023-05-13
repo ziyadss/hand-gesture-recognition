ad6c6f3766b389619cbc7d9033a5cf36ac8db7ec
Ziyad Sameh <ziyad.ss@hotmail.com>
2023-05-13 20:19:07 +0300

    Update extractor.py

--- extractor.py
+++ extractor.py
@@ -1,18 +1,19 @@
+import cv2
 import numpy as np
 
 from features import *
 
-import cv2
 
 def preprocess(img: np.ndarray) -> np.ndarray:
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img = cv2.resize(img, (64, 64))
     return img
 
+
 def get_features_from_path(path: str) -> np.ndarray:
     # Load image
     img = cv2.imread(path)
-    
+
     # Segment hand
     hand, mask = sgm(img)
 
