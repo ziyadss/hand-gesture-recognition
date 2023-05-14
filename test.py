5c19300a3f794e3dfb72a1bbdc4b97f6043b9fb6
Ziyad Sameh <ziyad.ss@hotmail.com>
2023-05-14 16:58:21 +0300

    Update test.py

--- test.py
+++ test.py
@@ -1,18 +1,28 @@
+import os
 import pickle
+import sys
 import time
 
 from sklearn.svm import SVC
 
 from extractor import get_features_from_path
 
-t_start = time.perf_counter()
-
 MODEL_FILENAME = "model_train.pkl"
 with open(MODEL_FILENAME, "rb") as f:
     clf: SVC = pickle.load(f)
 
+if len(sys.argv) != 2:
+    print("Usage: python test.py <folder_path>")
+    sys.exit(1)
 
-paths = []
+folder_path = sys.argv[1]
+paths = [
+    os.path.join(folder_path, file)
+    for file in os.listdir(folder_path)
+    if file.endswith(".jpg") or file.endswith(".JPG")
+]
+
+t_start = time.perf_counter()
 
 data = [get_features_from_path(path) for path in paths]
 
@@ -25,4 +35,4 @@ with open("predictions.csv", "w") as f:
 t_end = time.perf_counter()
 seconds = t_end - t_start
 
-print(f"Time: {seconds:.1f} seconds")
+print(f"Time: {seconds:.1f} seconds for {len(data)} images")
